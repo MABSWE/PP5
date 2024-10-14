3da87e7e07015bc57d13643a984f0459468aada9
Marat Akbar Boyev <maratboyev@outlook.com>
2024-10-14 19:57:06 +0000

    Add test image and change padding

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
