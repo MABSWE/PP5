15dda9724acb0987a4c54992f0c6d1c6b44a7f18
Marat Akbar Boyev <maratboyev@outlook.com>
2024-10-28 19:53:59 +0000

    Add glue, lashes, other

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('lashes/', views.lashes, name='lashes'), 
+    path('glue/', views.glue, name='glue'),
+    path('other/', views.other, name='other'), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
 from .models import Product
 
-# Create your views here.
-
+# Skapa dina vyer här.
 
 def all_products(request):
     """ Show products and searching and sorting queries """
-
     products = Product.objects.all()
-
     context = {
         'products': products,
-}
-    
+    }
     return render(request, 'products/products.html', context)
+
+def lashes(request):
+    """ Visa endast fransar """
+    lashes_products = Product.objects.filter(category__name='Lashes')
+    context = {
+        'lashes': lashes_products,
+    }
+    return render(request, 'products/lashes.html', context)
+
+def glue(request):
+    """ Visa endast limprodukter """
+    glue_products = Product.objects.filter(category__name='Glue')
+    context = {
+        'glue': glue_products,
+    }
+    return render(request, 'products/glue.html', context)
+
+def other(request):
+    """ Visa övriga produkter """
+    other_products = Product.objects.filter(category__name='Other')
+    context = {
+        'other': other_products,
+    }
+    return render(request, 'products/other.html', context)
