aec37af653eee1b3ffdd429a636901c499921661
Marat Akbar Boyev <maratboyev@outlook.com>
2024-11-17 18:37:17 +0000

    Add newsletter, tersimonial and contact form models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Testimonial(models.Model):
+    user_name = models.CharField(max_length=100)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user_name
+
+class NewsletterSubscription(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    sent_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name}"
