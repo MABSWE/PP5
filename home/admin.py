c8b627a54c0483561698f8e2324e7e1b8f58ac58
Marat Akbar Boyev <maratboyev@outlook.com>
2024-11-17 18:40:13 +0000

    Register all models in the admin panel, run migrations

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import NewsletterSubscription, Testimonial, ContactMessage
+
 
 # Register your models here.
+admin.site.register(NewsletterSubscription)
+admin.site.register(Testimonial)
+admin.site.register(ContactMessage)
