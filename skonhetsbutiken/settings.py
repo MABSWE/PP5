ab8939bedb298d8e442f07b337bce3149b82e985
Marat Akbar Boyev <maratboyev@outlook.com>
2024-12-04 19:05:31 +0000

    Add WhiteNoise for serving static and media files

--- skonhetsbutiken/settings.py
+++ skonhetsbutiken/settings.py
@@ -2,7 +2,7 @@ import os
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Google Maps API key
 GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,3 +156,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# WhiteNoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
